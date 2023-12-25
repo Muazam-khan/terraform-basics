c68d4ee6ac93f0607c27b5c3c93c8ae570443dbb
Muazam-khan <kmuazam6@gmail.com>
2023-12-25 17:04:29 -0500

    Auto Push terraform-basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app" { # aws_instance is a keyword, web is also a keyword, if u chang web to app, it ll b name of aws instanc ea sper terraform
-  ami           = ami-0f75a13ad2e340a58
+  ami           = "ami-0f75a13ad2e340a58"
   instance_type = "t2.micro"
 
   tags = {
