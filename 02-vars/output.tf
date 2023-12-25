267e0dede279146eddd4e51a027a630ead791baf
Muazam-khan <kmuazam6@gmail.com>
2023-12-25 12:17:15 -0500

    Auto Push terraform-basics

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -52,5 +52,5 @@ variable "training-map" {
 }
 # Printing a map variable
 output "training-map-op" {
-    value =   "${var.training-map[training]}"
+    value =   "${var.training-map{training}} has great scope of opprtunities"
 }
