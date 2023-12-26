7e8475842d64934236f70ebbc50a685fabc40b52
Muazam-khan <kmuazam6@gmail.com>
2023-12-25 22:07:54 -0500

    Auto Push terraform-basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" { # aws_instance is a keyword, web is also a keyword, if u chang web to app, it ll b name of aws instanc ea sper terraform
+resource "aws_instance" "app" { # aws_instance is a keyword, web is also a keyword, if u chang web to app, it ll b name of aws instanc ea sper terraform
   ami           = "ami-0f75a13ad2e340a58"  # argument
   instance_type = "t3.micro"
 
@@ -8,9 +8,9 @@ resource "aws_instance" "web" { # aws_instance is a keyword, web is also a keywo
 }
 
 output "instance_dns" {     # attribute
-   value = aws_instance.web.private_dns
+   value = aws_instance.app.private_dns
 }
 
 output "instance_arn" {     # attribute
-   value = aws_instance.web.arn
+   value = aws_instance.app.arn
 }
