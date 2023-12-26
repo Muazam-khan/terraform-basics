7dd4b92d2c9762d221a7e1a092e9baf1300d23f7
Muazam-khan <kmuazam6@gmail.com>
2023-12-25 22:00:25 -0500

    Auto Push terraform-basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -8,9 +8,9 @@ resource "aws_instance" "app" { # aws_instance is a keyword, web is also a keywo
 }
 
 output "instance_dns" {     # attribute
-   value = "aws_instance.app.private_dns"
+   value = aws_instance.app.private_dns
 }
 
 output "instance_arn" {     # attribute
-   value = "aws_instance.app.arn"
+   value = aws_instance.app.arn
 }
