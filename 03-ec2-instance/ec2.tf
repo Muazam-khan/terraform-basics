6fb767996c1fc789195a079aecbb13a3fba44b1c
Muazam-khan <kmuazam6@gmail.com>
2024-01-03 17:00:44 -0500

    Auto Push terraform-basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -14,3 +14,27 @@ output "instance_dns" {     # attribute
 output "instance_arn" {     # attribute
    value = aws_instance.app.arn
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "b56_allow_tls"
+  description = "B56 Allow TLS inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
